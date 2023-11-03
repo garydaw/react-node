55d9a2864889a7d7c55b592abbc3e880319ffc6c
Gary <garydaw@ntlworld.com>
2023-11-03 21:11:22 +0000

    connected to the db!

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const mariadb = require('mariadb');
 const app = express();
-const players = [{ally_code:"1",name:"Bobba"},{ally_code:"2",name:"Yoda"},{ally_code:"3",name:"Ben"},{ally_code:"4",name:"Luke"}]
 app.use(cors());
 
-app.get('/api/players/:ally_code', (req, res) => {
-    const ally_code = req.params.ally_code;
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '1pig2cowes3sheep',
+  database: 'swgoh'
+});
+
+const players = [{ally_code:"1",name:"Bobba"},{ally_code:"2",name:"Yoda"},{ally_code:"3",name:"Ben"},{ally_code:"4",name:"Luke"}]
+
+app.get('/api/players/:ally_code', async (req, res) => {
+
+  const conn = await pool.getConnection();
+  const rows = await conn.query("SELECT * FROM player");
+  conn.release();
+  res.status(200).json(rows);
+
+  /*
+  const ally_code = req.params.ally_code;
     
     const result = players.filter(player => player.ally_code === ally_code);
 
     res.status(201).json(result);
+  */
   });
 
-/*
-app.get('/api/players', (req, res) => {
 
-  res.status(201).json(players);
-});*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

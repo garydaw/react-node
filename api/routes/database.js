84f60ce88bf5aa3a5c6fa18684ab396ded65809a
Gary <garydaw@ntlworld.com>
2023-11-10 12:58:44 +0000

    new db structure

--- api/routes/database.js
+++ api/routes/database.js
@@ -9,61 +9,63 @@ router.get('/migrate', async (req, res) => {
     console.log("starting");
     console.log("creating player");
     await runSQL("CREATE TABLE IF NOT EXISTS player ("+
-        "id int auto_increment, "+
-        "ally_code VARCHAR(64) NOT NULL UNIQUE, "+
-        "`name` VARCHAR(64), "+
-        "primary key(id));");
+        "ally_code int NOT NULL, "+
+        "ally_name VARCHAR(64), "+
+        "character_galactic_power int, "+
+        "ship_galactic_power int, "+
+        "guild_id varchar(32), "+
+        "guild_name varchar(256), "+
+        "primary key(ally_code));");
   
     console.log("creating unit");
     await runSQL("CREATE TABLE IF NOT EXISTS unit ("+
-        "id int AUTO_INCREMENT, "+
         "base_id VARCHAR(64) NOT NULL, "+
         "combat_type int, "+
-        "`name` VARCHAR(128), "+
+        "character_name VARCHAR(128), "+
         "url VARCHAR(256), "+
-        "primary key(id), "+
-        "UNIQUE(base_id));");
-  
-    console.log("creating unit_mods");
-    await runSQL("CREATE TABLE IF NOT EXISTS unit_mods ("+
-        "id int AUTO_INCREMENT, "+
-        "date DATE NOT NULL DEFAULT CURDATE(), "+
-        "unit_id int NOT NULL, "+
-        "arrow VARCHAR(64), "+
-        "triangle VARCHAR(64), "+
-        "circle VARCHAR(64), "+
-        "`cross` VARCHAR(64), "+
-        "primary key(id), "+
-        "UNIQUE(DATE,unit_id), "+
-        "CONSTRAINT fk_unit_mods__unit "+
-        "FOREIGN KEY (unit_id) REFERENCES unit (id) "+
-        "ON DELETE CASCADE "+
-        "ON UPDATE RESTRICT);");
+        "primary key(base_id));");
   
     console.log("creating player_unit");
     await runSQL("CREATE TABLE IF NOT EXISTS player_unit ("+
-        "player_id int, "+
-        "unit_id int NOT NULL, "+
-        "level int, "+
+        "ally_code int NOT NULL, "+
+        "base_id VARCHAR(64) NOT NULL, "+
         "gear_level int, "+
+        "level int, "+
         "power int, "+
-        "arrow VARCHAR(64), "+
-        "triangle VARCHAR(64), "+
-        "circle VARCHAR(64), "+
-        "`cross` VARCHAR(64), "+
-        "square_set int, "+
-        "arrow_set int, "+
-        "diamond_set int, "+
-        "triangle_set int, "+
-        "circle_set int, "+
-        "cross_set int, "+
-        "primary key(player_id, unit_id), "+
+        "rarity int, "+
+        "primary key(ally_code, base_id), "+
         "CONSTRAINT fk_player_unit__player "+
-        "FOREIGN KEY (player_id) REFERENCES player (id) "+
+        "FOREIGN KEY (ally_code) REFERENCES player (ally_code) "+
         "ON DELETE CASCADE "+
         "ON UPDATE RESTRICT, "+
         "CONSTRAINT fk_player_unit__unit "+
-        "FOREIGN KEY (unit_id) REFERENCES unit (id) "+
+        "FOREIGN KEY (base_id) REFERENCES unit (base_id) "+
+        "ON DELETE CASCADE "+
+        "ON UPDATE RESTRICT);");
+  
+    console.log("creating player_mods");
+    await runSQL("CREATE TABLE IF NOT EXISTS player_mods ("+
+        "id varchar(32) NOT NULL, "+
+        "ally_code int NOT NULL, "+
+        "base_id VARCHAR(64) NOT NULL, "+
+        "level int NOT NULL, "+
+        "tier int NOT NULL, "+
+        "rarity int NOT NULL, "+
+        "slot int NOT NULL, "+
+        "group_set int NOT NULL, "+
+        "primary_stat varchar(64) NOT NULL, "+
+        "primary_stat_value varchar(16) NOT NULL, "+
+        "secondary_stat_1 varchar(64) NOT NULL, "+
+        "secondary_stat_1_value varchar(16) NOT NULL, "+
+        "secondary_stat_2 varchar(64) NOT NULL, "+
+        "secondary_stat_2_value varchar(16) NOT NULL, "+
+        "secondary_stat_3 varchar(64) NOT NULL, "+
+        "secondary_stat_3_value varchar(16) NOT NULL, "+
+        "secondary_stat_4 varchar(64) NOT NULL, "+
+        "secondary_stat_4_value varchar(16) NOT NULL, "+
+        "primary key(id), "+
+        "CONSTRAINT fk_player_mods__player_unit "+
+        "FOREIGN KEY (ally_code, base_id) REFERENCES player_unit (ally_code, base_id) "+
         "ON DELETE CASCADE "+
         "ON UPDATE RESTRICT);");
   

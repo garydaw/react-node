d1d140e88185866440d367c6168b775141b2cd5e
Gary <garydaw@ntlworld.com>
2024-02-01 14:17:50 +0000

    change password

--- src/Main.js
+++ src/Main.js
@@ -12,6 +12,7 @@ export default function Main({logoutHandler}) {
     const [playerData, setPlayerData] = useState({});
     const { showLoading, hideLoading } = useLoading();
     const { showError } = useError();
+    const [passwordError, setPasswordError] = useState("");
     const helpText = "Search for you player using you ally code. This can be found by clicking on your name from the home screen.";
 
     //does user have access to admin
@@ -121,6 +122,44 @@ export default function Main({logoutHandler}) {
         }
       }
 
+      let passwordHandler = async () =>
+      {
+        setPasswordError("");
+        const current_password = document.getElementById("current_password").value;
+        const password1 = document.getElementById("password1").value;
+        const password2 = document.getElementById("password2").value;
+        const username = localStorage.getItem('ally_code');
+
+        if(password1 === ""){
+          setPasswordError("Passwords cannot be blank");
+          return;
+        }
+        if(password1 !== password2){
+          setPasswordError("New Passwords do not match!");
+          return;
+        }
+        /*
+        const response = await fetch(process.env.REACT_APP_API_URL + '/player/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
+      
+        if (response.ok) {
+          const data = await response.json();
+          setIsLoggedIn(true);
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('access', data.user.access);
+          localStorage.setItem('ally_code', data.user.ally_code);
+        } else {
+          // Handle login failure
+          const errorData = await response.json();
+          setPasswordError(errorData.message);
+    }*/
+      }
+
     return (
            
           <div className="container-full p-3">
@@ -151,7 +190,7 @@ export default function Main({logoutHandler}) {
                     <ul className="dropdown-menu dropdown-menu-end">
                       {access === "1" && <li><a className="dropdown-item" href="#" onClick={getUnits}>Refresh Units</a></li>}
                       {access === "1" && <li><a className="dropdown-item" href="#" onClick={getBestMods}>Refresh Mods</a></li>}
-                      <li><a className="dropdown-item" href="#">Change Password</a></li>
+                      <li><a className="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#password_modal">Change Password</a></li>
                       <li><a className="dropdown-item" href="#" onClick={logoutHandler}>Logout</a></li>
                     </ul>
                   </div>
@@ -161,7 +200,45 @@ export default function Main({logoutHandler}) {
             <div className="row">
               <Player playerData={playerData} refreshPlayerData={refreshPlayerData}></Player>
             </div>
+
+           
+            <div className="modal fade" id="password_modal" tabIndex="-1" aria-labelledby="password_modal_Label" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="password_modal_Label">Change Password</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-body">
+                    <p>This site is NOT https, so please dont use a usual password as it is not secure!</p>
+                    <div className="row">
+                      <div className="col-10 offset-1 card d-show mt-5 pb-3">
+                          <div className="mb-3">
+                              <label htmlFor="current_password" className="form-label">Current Password</label>
+                              <input type="text" className="form-control" id="current_password" placeholder="Enter your Current Password" required></input>
+                          </div>
+                          <div className="mb-3">
+                              <label htmlFor="password1" className="form-label">New Password</label>
+                              <input type="password" className="form-control" id="password1" placeholder="Enter your new password" required></input>
+                          </div>
+                          <div className="mb-3">
+                              <label htmlFor="password2" className="form-label">Re-Type New Password</label>
+                              <input type="password" className="form-control" id="password2" placeholder="Re-type your new password" required></input>
+                          </div>
+                          <div className={passwordError === "" ? "d-none" : "d-show pb-3 text-danger"}>
+                            {passwordError}
+                          </div>
+                          <button type="submit" onClick={passwordHandler} className="btn btn-primary btn-block">Change Password</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
-    
       );
 }

ba4fb5b708fbac970d8dd176c1e8c73d410a7d3c
Gary <garydaw@ntlworld.com>
2024-03-08 20:48:31 +0000

    build

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use('/api',apiRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

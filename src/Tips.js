07be7382ebbc310eaa8fb054250d184d7cc6cc3c
Gary <garydaw@ntlworld.com>
2024-03-29 16:36:34 +0000

    Relic mats update

--- src/Tips.js
+++ src/Tips.js
@@ -8,16 +8,73 @@ export default function Tips() {
             </ul>
             <h3>Relic Materials</h3>
             Best ways to get relic materials
-            <ul>
-                <li>Carbonite Circuit Board - Crystals?</li>
-                <li>Bronzium Wiring - Scavenger list by highest Quantity</li>
-                <li>Cromium Transistor - MK 7 BlasTech Weapon Mod can often be found in Shipments-&gt; Guild Activiy, then be exchanged by Scavenger</li>
-                <li>Aurodium Heatsink - Scavenger</li>
-                <li>Electrium Conductor - Shipments-&gt; Guild Activiy 2 or 3 for 200 or 300 Mk III Raid Tokens</li>
-                <li>Zinbiddle Card - Shipments-&gt; Guild Activiy 2 or 3 for 250 or 375 Mk III Raid Tokens</li>
-                <li>Impulse Detector - Shipments-&gt; Guild Activiy 2 or 3 for 530 or 795 Mk III Raid Tokens</li>
-                <li>Aeromagnifier - Shipments-&gt; Guild Activiy 2 or 3 for 400 or 600 Mk III Raid Tokens</li>
-            </ul>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Relic Material</th>
+                        <th>Location</th>
+                        <th>Gear</th>
+                        <th>Tip</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Carbonite Circuit Board</td>
+                        <td>Light Side Battle Normal 1-D</td>
+                        <td>
+                            <ul>
+                                <li>MK1 Baw Armor Mod</li>
+                                <li>MK1 Fabritech Data Pad</li>
+                                <li>MK1 CEC Fusion Furnace</li>
+                                <li>MK1 Loronar Power Cell</li>
+                            </ul>
+                        </td>
+                        <td>Just Farm away</td>
+                    </tr>
+                    <tr>
+                        <td>Bronzium Wiring</td>
+                        <td>Light Side Battle Normal 7-B</td>
+                        <td>MK5 Sorosuub Keypad Salvage</td>
+                        <td>Bonus of Kyrotechs from this level</td>
+                    </tr>
+                    <tr>
+                        <td>Cromium Transistor</td>
+                        <td>Shipments -&gt; Guild Activiy -&gt; 150 Guild tokens for 10</td>
+                        <td>MK 7 BlasTech Weapon Mod</td>
+                        <td>10 is worth about 33 Transistors!</td>
+                    </tr>
+                    <tr>
+                        <td>Aurodium Heatsink</td>
+                        <td>Shipments -&gt; Guild Activiy -&gt; 1,625 MK1 Raid tokens for 125</td>
+                        <td>MK 3 Sienar Holo Projector Salvage</td>
+                        <td>In Scavenger, click into Mk 2 Sienar Holo Project, then craft your 125 salavage into 6 full. Worth 25 Heatsinks!</td>
+                    </tr>
+                    <tr>
+                        <td>Electrium Conductor</td>
+                        <td>Shipments-&gt; Guild Activiy 2 or 3 for 200 or 300 Mk III Raid Tokens</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Zinbiddle Card</td>
+                        <td>Shipments-&gt; Guild Activiy 2 or 3 for 250 or 375 Mk III Raid Tokens</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Impulse Detector</td>
+                        <td>Shipments-&gt; Guild Activiy 2 or 3 for 530 or 795 Mk III Raid Tokens</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Aeromagnifier</td>
+                        <td>Shipments-&gt; Guild Activiy 2 or 3 for 400 or 600 Mk III Raid Tokens</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
